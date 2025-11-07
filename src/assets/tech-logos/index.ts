133425edc05c4d23f1a12252cf0a54d0ddf29d31
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-11-07 20:15:22 +0800

    chore: add tech logos to assets folder

---
+++ src/assets/tech-logos/index.ts
@@ -0,0 +1,23 @@
+import htmlLogo from './html-5-logo.svg';
+import cssLogo from './css-3-logo.svg';
+import javascriptLogo from './javascript-logo.svg';
+import typescriptLogo from './typescript-logo.svg';
+import reactLogo from './react-logo.svg';
+import twLogo from './tailwind-logo.svg';
+import viteLogo from './vite-logo.svg';
+import webpackLogo from './webpack-logo.svg';
+import vitestLogo from './vitest-logo.svg';
+import jestLogo from './jest-logo.svg';
+
+export const techLogos = [
+  htmlLogo,
+  cssLogo,
+  javascriptLogo,
+  typescriptLogo,
+  reactLogo,
+  twLogo,
+  viteLogo,
+  webpackLogo,
+  vitestLogo,
+  jestLogo,
+];

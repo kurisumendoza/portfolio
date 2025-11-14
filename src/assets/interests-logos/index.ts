4b34d8ad94fcb7ec93fc870dc7aa36913e01e142
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-11-14 17:40:54 +0800

    feat: add logos for interests

---
+++ src/assets/interests-logos/index.ts
@@ -0,0 +1,19 @@
+import videoGamesLogo from './video-games-logo.svg';
+import animeLogo from './anime-logo.svg';
+import moviesLogo from './movies-logo.svg';
+import tvSeriesLogo from './tv-series-logo.svg';
+import musicLogo from './music-logo.svg';
+import collectiblesLogo from './collectibles-logo.svg';
+import foodLogo from './food-logo.svg';
+import catsLogo from './cats-logo.svg';
+
+export const interestsLogos = [
+  videoGamesLogo,
+  animeLogo,
+  moviesLogo,
+  tvSeriesLogo,
+  musicLogo,
+  collectiblesLogo,
+  foodLogo,
+  catsLogo,
+];

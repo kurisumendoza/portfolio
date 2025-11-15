101277103019a185db2684fa1d61bac7f3972994
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-11-16 03:34:47 +0800

    feat: add video games images for AboutInterests

---
+++ src/assets/pictures/interests/video-games/index.ts
@@ -0,0 +1,33 @@
+import eldenRingImg from './elden-ring.webp';
+import bloodborneImg from './bloodborne.webp';
+import darkSoulsImg from './dark-souls.webp';
+import darkSouls2Img from './dark-souls-2.webp';
+import darkSouls3Img from './dark-souls-3.webp';
+import sekiroImg from './sekiro.webp';
+import hollowKnightImg from './hollow-knight.webp';
+import persona4GoldenImg from './persona-4-golden.webp';
+import persona5Img from './persona-5.webp';
+import ff7RemakeImg from './final-fantasy-vii-remake.webp';
+import ffxvImg from './final-fantasy-xv.webp';
+import niohImg from './nioh.webp';
+import nioh2Img from './nioh-2.webp';
+import rdr2Img from './red-dead-redemption-2.webp';
+import ragnarokImg from './ragnarok-online.webp';
+
+export const gamingImgs = [
+  eldenRingImg,
+  bloodborneImg,
+  darkSoulsImg,
+  darkSouls2Img,
+  darkSouls3Img,
+  sekiroImg,
+  hollowKnightImg,
+  persona4GoldenImg,
+  persona5Img,
+  ff7RemakeImg,
+  ffxvImg,
+  niohImg,
+  nioh2Img,
+  rdr2Img,
+  ragnarokImg,
+];

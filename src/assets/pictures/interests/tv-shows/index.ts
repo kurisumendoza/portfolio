4a258adea316891de77da3a3585b5d126fee4259
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-11-16 11:56:00 +0800

    feat: add tv show images for AboutInterests

---
+++ src/assets/pictures/interests/tv-shows/index.ts
@@ -0,0 +1,33 @@
+import betterCallSaulImg from './better-call-saul.webp';
+import breakingBadImg from './breaking-bad.webp';
+import avatarImg from './avatar.webp';
+import falloutImg from './fallout.webp';
+import lifeGiveTangerinesImg from './life-give-tangerine.webp';
+import lokiImg from './loki.webp';
+import spongebobImg from './spongebob.webp';
+import aliceInBorderlandImg from './alice-in-borderland.webp';
+import squidGameImg from './squid-game.webp';
+import theBoysImg from './the-boys.webp';
+import strangerThingsImg from './stranger-things.webp';
+import gameOfThronesImg from './game-of-thrones.webp';
+import theGloryImg from './the-glory.webp';
+import phineasAndFerbImg from './phineas-and-ferb.webp';
+import boysOverFlowersImg from './boys-over-flowers.webp';
+
+export const tvShowsImgs = [
+  betterCallSaulImg,
+  breakingBadImg,
+  avatarImg,
+  falloutImg,
+  lifeGiveTangerinesImg,
+  lokiImg,
+  spongebobImg,
+  aliceInBorderlandImg,
+  squidGameImg,
+  theBoysImg,
+  strangerThingsImg,
+  gameOfThronesImg,
+  theGloryImg,
+  phineasAndFerbImg,
+  boysOverFlowersImg,
+];

f607b2e70aa88e11f97b675e1bf1c2ba7dd9dab4
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-11-17 02:03:59 +0800

    feat: add books and manga images for AboutInterests

---
+++ src/assets/pictures/interests/books/index.ts
@@ -0,0 +1,5 @@
+import harryPotterImg from './harry-potter.jpg';
+import hungerGamesImg from './hunger-games.jpg';
+import narniaImg from './chronicles-of-narnia.jpg';
+
+export const booksImgs = [harryPotterImg, hungerGamesImg, narniaImg];
---
+++ src/assets/pictures/interests/manga/index.ts
@@ -0,0 +1,5 @@
+import onePieceImg from './one-piece.jpg';
+import fullMetalAlchemistImg from './full-metal-alchemist.jpg';
+import magiImg from './magi.jpg';
+
+export const mangaImgs = [onePieceImg, fullMetalAlchemistImg, magiImg];

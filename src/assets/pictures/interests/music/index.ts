f605d7c7c63e207ade903668ae98abc9a549cb03
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-11-16 13:43:35 +0800

    feat: add album images with names for AboutInterests

---
+++ src/assets/pictures/interests/music/index.ts
@@ -0,0 +1,33 @@
+import jojiImg from './joji.webp';
+import lisaImg from './lisa.webp';
+import newJeansImg from './new-jeans.webp';
+import miletImg from './milet.webp';
+import aimerImg from './aimer.webp';
+import clairoImg from './clairo.webp';
+import illitImg from './illit.webp';
+import misamoImg from './misamo.webp';
+import scandalBandImg from './scandal-band.webp';
+import paramoreImg from './paramore.webp';
+import eraserheadsImg from './eraserheads.webp';
+import rookiezIsPunkdImg from './rookiez-is-punkd.webp';
+import boysLikeGirlsImg from './boys-like-girls.webp';
+import kafImg from './kaf.webp';
+import orangeAndLemonsImg from './orange-and-lemons.webp';
+
+export const musicImgs = [
+  { name: 'Joji', src: jojiImg },
+  { name: 'LiSA', src: lisaImg },
+  { name: 'New Jeans', src: newJeansImg },
+  { name: 'Milet', src: miletImg },
+  { name: 'Aimer', src: aimerImg },
+  { name: 'Clairo', src: clairoImg },
+  { name: 'Illit', src: illitImg },
+  { name: 'Misamo', src: misamoImg },
+  { name: 'Scandal', src: scandalBandImg },
+  { name: 'Paramore', src: paramoreImg },
+  { name: 'Eraserheads', src: eraserheadsImg },
+  { name: "Rookiez is Punk'd", src: rookiezIsPunkdImg },
+  { name: 'Boys Like Girls', src: boysLikeGirlsImg },
+  { name: 'Kaf', src: kafImg },
+  { name: 'Orange and Lemons', src: orangeAndLemonsImg },
+];

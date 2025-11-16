3782094c61ea23d87690938ef59025faca5ad210
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-11-16 09:42:27 +0800

    feat: add anime images for AboutInterests

---
+++ src/assets/pictures/interests/anime/index.ts
@@ -0,0 +1,33 @@
+import codeGeassImg from './code-geass.jpg';
+import attackOnTitanImg from './attack-on-titan.jpg';
+import fullMetalAlchemistImg from './full-metal-alchemist.jpg';
+import onePieceImg from './one-piece.jpg';
+import deathNoteImg from './death-note.jpg';
+import fateStayNightImg from './fate-stay-night.jpg';
+import kurokoBasketballImg from './kuroko-basketball.jpg';
+import reZeroImg from './re-zero.jpg';
+import narutoImg from './naruto.jpg';
+import steinsGateImg from './steins-gate.jpg';
+import jojoBizarreAdventureImg from './jojo-bizarre-adventures.jpg';
+import blackCloverImg from './black-clover.jpg';
+import berserkImg from './berserk.jpg';
+import yourNameImg from './your-name.jpg';
+import myHeroAcademiaImg from './my-hero-academia.jpg';
+
+export const animeImgs = [
+  codeGeassImg,
+  attackOnTitanImg,
+  fullMetalAlchemistImg,
+  onePieceImg,
+  deathNoteImg,
+  fateStayNightImg,
+  kurokoBasketballImg,
+  reZeroImg,
+  narutoImg,
+  steinsGateImg,
+  jojoBizarreAdventureImg,
+  blackCloverImg,
+  berserkImg,
+  yourNameImg,
+  myHeroAcademiaImg,
+];

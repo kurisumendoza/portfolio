aefb43386b7ff114350c42f33abf511263e1a94a
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-11-16 10:42:31 +0800

    feat: add movie images for AboutInterests

---
+++ src/assets/pictures/interests/movies/index.ts
@@ -0,0 +1,33 @@
+import harryPotterImg from './harry-potter.webp';
+import lordOfTheRingsImg from './lord-of-the-rings.webp';
+import greatGatsbyImg from './great-gatsby.webp';
+import enemyImg from './enemy.webp';
+import inceptionImg from './inception.webp';
+import interstellarImg from './interstellar.webp';
+import darkKnightImg from './dark-knight.webp';
+import avengersEndgameImg from './avengers-endgame.webp';
+import matrixImg from './matrix.webp';
+import piratesOfCaribbeanImg from './pirates-of-caribbean.webp';
+import eleanorRigbyImg from './eleanor-rigby.webp';
+import spotlessMindImg from './spotless-mind.webp';
+import trumanShowImg from './truman-show.webp';
+import wolfOfWallStreetImg from './wolf-of-wall-street.webp';
+import frankImg from './frank.webp';
+
+export const moviesImgs = [
+  harryPotterImg,
+  lordOfTheRingsImg,
+  greatGatsbyImg,
+  enemyImg,
+  inceptionImg,
+  interstellarImg,
+  darkKnightImg,
+  avengersEndgameImg,
+  matrixImg,
+  piratesOfCaribbeanImg,
+  eleanorRigbyImg,
+  spotlessMindImg,
+  trumanShowImg,
+  wolfOfWallStreetImg,
+  frankImg,
+];

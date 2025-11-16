618a84a89af5fb30c743982e387af3315031c4a8
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-11-16 11:00:56 +0800

    feat: replace JPG assets with WebP and update imports

--- src/assets/pictures/interests/anime/index.ts
+++ src/assets/pictures/interests/anime/index.ts
@@ -1,18 +1,18 @@
-import codeGeassImg from './code-geass.jpg';
-import attackOnTitanImg from './attack-on-titan.jpg';
-import fullMetalAlchemistImg from './full-metal-alchemist.jpg';
-import onePieceImg from './one-piece.jpg';
-import deathNoteImg from './death-note.jpg';
-import fateStayNightImg from './fate-stay-night.jpg';
-import kurokoBasketballImg from './kuroko-basketball.jpg';
-import reZeroImg from './re-zero.jpg';
-import narutoImg from './naruto.jpg';
-import steinsGateImg from './steins-gate.jpg';
-import jojoBizarreAdventureImg from './jojo-bizarre-adventures.jpg';
-import blackCloverImg from './black-clover.jpg';
-import berserkImg from './berserk.jpg';
-import yourNameImg from './your-name.jpg';
-import myHeroAcademiaImg from './my-hero-academia.jpg';
+import codeGeassImg from './code-geass.webp';
+import attackOnTitanImg from './attack-on-titan.webp';
+import fullMetalAlchemistImg from './full-metal-alchemist.webp';
+import onePieceImg from './one-piece.webp';
+import deathNoteImg from './death-note.webp';
+import fateStayNightImg from './fate-stay-night.webp';
+import kurokoBasketballImg from './kuroko-basketball.webp';
+import reZeroImg from './re-zero.webp';
+import narutoImg from './naruto.webp';
+import steinsGateImg from './steins-gate.webp';
+import jojoBizarreAdventureImg from './jojo-bizarre-adventures.webp';
+import blackCloverImg from './black-clover.webp';
+import berserkImg from './berserk.webp';
+import yourNameImg from './your-name.webp';
+import myHeroAcademiaImg from './my-hero-academia.webp';
 
 export const animeImgs = [
   codeGeassImg,
